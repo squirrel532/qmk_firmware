b661ebc0092be554141caaa901cae2f1a56a5dbd
Larry Huang <squirrel532@plurk.com>
2023-02-12 01:38:08 +0800

    Add my keymap

--- keyboards/bioi/g60ble/rules.mk
+++ keyboards/bioi/g60ble/rules.mk
@@ -7,6 +7,14 @@ F_CPU = 8000000
 # Bootloader selection
 BOOTLOADER = qmk-dfu
 
+SRC += usart.c \
+       ble.c \
+       main.c
+
+OPT_DEFS += -DPROTOCOL_BLE
+OPT_DEFS += -DUART_RX1_BUFFER_SIZE=16 -DUART_TX1_BUFFER_SIZE=16
+OPT_DEFS += -DUSART1_ENABLED
+
 # Build Options
 #   change yes to no to disable
 #
@@ -17,7 +25,8 @@ CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = yes       # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = yes
 
 LAYOUTS = 60_ansi 60_iso 60_hhkb 60_ansi_split_bs_rshift 60_tsangan_hhkb
